a4d344222c57fc4bef7b543c8fb77f7307ea568c
robert <robert_tseng@mail.chungyo.net>
2022-03-03 17:48:03 +0800

    add guess number

--- app/src/main/java/com/tseng/bmi2/MainActivity.kt
+++ app/src/main/java/com/tseng/bmi2/MainActivity.kt
@@ -2,10 +2,42 @@ package com.tseng.bmi2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.tseng.bmi2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var secretNum = (1..100).random()
+    var min = 1
+    var max = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun bmi(view: View) {
+        var weight = binding.edWeight.text.toString().toFloat()
+        var height = binding.edHeight.text.toString().toFloat()
+        println("Your BMI is ${weight/(height*height)}")
+    }
+
+    fun guess(view: View) {
+        var inputNum = binding.edNumber.text.toString().toInt()
+        if (inputNum < min || inputNum > max) {
+            println("Funny?")
+            return
+        }
+        if (inputNum > secretNum) {
+            max = inputNum
+            println("Try $min ~ $max")
+        } else if (inputNum < secretNum) {
+            min = inputNum
+            println("Try $min ~ $max")
+        } else {
+            secretNum = (1..100).random()
+            println("Well done.")
+        }
     }
 }
